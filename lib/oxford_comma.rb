06ce7bcc8b6c5e6c5ddbeb3844f8943a4468c3a0
Jesse Wenger <wengerj@gmail.com>
2021-03-03 03:06:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  size = array.size
+  if size == 1 
+    array.join
+  elsif size == 2 
+    array.join(" and ")
+  elsif size >= 3
+    last_item = array.pop
+    array.join(", ") << ", and #{last_item}"
+  end
 end
