598b60610f899db603a3fca7bb42656bdb59a834
Jesse Wenger <wengerj@gmail.com>
2021-03-03 16:47:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   size = array.size
   if size == 1 
     array.join
@@ -7,5 +8,16 @@ def oxford_comma(array)
   elsif size >= 3
     last_item = array.pop
     array.join(", ") << ", and #{last_item}"
+=======
+  case array.size
+  when 1 
+    array.join
+  when 2 
+    array.join(" and ")
+  when 3
+    last_item = array.pop
+    array.join(", ") << ", and #{last_item}"
+  when 4
+>>>>>>> da00aad90455ea53c71cb8aa706f80632d52f2ee
   end
 end
